3708cce53f6ffb02de41af6722a2a2ed74270bc9
Ape <kustomape@gmail.com>
2020-06-27 03:19:29 +0900

    Adds chapter01 section.

---
+++ .history/chapter01/strings_01_20200519114436.py
@@ -0,0 +1,24 @@
+# 文字リテラルを記述する
+'文字列'
+"文字列"
+
+# 文字列は英数字/空白/日本語、全角でも大丈夫です
+"Hello, Python"
+"myname"
+"こんにちは"
+"2019"
+
+# ダブルクオーテーションとシングルクオーテーションで囲った場合で違いはありません。
+# "myname"
+# myname = 変数
+# "2019" > 2019 = 整数値
+
+print('hello python')
+print('my name is Aenky')
+
+# 文字列の中でシングルクオートを使う
+# Tom's vehicle
+"Tom's vehicle"
+
+print("Tom's vehicle")
+print('彼は"こんにちは"と言いました。')
---
+++ .history/chapter01/strings_02_20200519114610.py
@@ -0,0 +1,19 @@
+# エスケープシーケンス
+# Windows環境では¥円マーク、その他では\バックスラッシュを使います。
+
+# ¥newline	文字列を途中で改行する
+# ¥¥	バックスラッシュ (¥)
+# ¥'	一重引用符 (')
+# ¥"	二重引用符 (")
+# ¥a	ASCII 端末ベル (BEL)
+# ¥b	ASCII バックスペース (BS)
+# ¥f	ASCII フォームフィード (FF)
+# ¥n	ASCII 行送り (LF)
+# ¥r	ASCII 復帰 (CR)
+# ¥t	ASCII 水平タブ (TAB)
+# ¥v	ASCII 垂直タブ (VT)
+# ¥ooo	8進数oooを持つASCII文字
+# ¥xhh	16進数hhを持つASCII文字
+# ¥N{name}	Unicode データベース中で名前 name を持つ文字
+# ¥uxxxx	16ビットの16進数値xxxxを持つUnicode文字
+# ¥Uxxxxxxxx	32ビットの16進数値xxxxxxxxを持つUnicode文字
